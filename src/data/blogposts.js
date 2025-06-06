f9741dc2e63f472be6207315cb6009f8de69c727
gabriele-ag <gabrieleardizzone.gdesigner@gmail.com>
2025-06-06 15:57:13 +0200

    feat: created input on main

---
+++ src/data/blogposts.js
@@ -0,0 +1,24 @@
+const blogPosts = [
+  {
+    id: 1,
+    title: "Torta di cioccolato",
+    description: "Una ricetta semplice e deliziosa per una torta di cioccolato al forno"
+  },
+  {
+    id: 2,
+    title: "Spaghetti alla carbonara",
+    description: "La ricetta classica degli spaghetti alla carbonara, con uova, guanciale e parmigiano"
+  },
+  {
+    id: 3,
+    title: "Pollo al curry",
+    description: "Un piatto indiano semplice e saporito, con pollo, curry e riso"
+  },
+  {
+    id: 4,
+    title: "Tiramisù",
+    description: "Il dolce italiano per eccellenza, con mascarpone, caffè e biscotti"
+  }
+];
+
+export default blogPosts
